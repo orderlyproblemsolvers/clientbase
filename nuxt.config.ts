8b649b6f70fa6364c24a23e9d64c2f7642a4cfbd
orderlyproblemsolvers <orderlyproblemsolvers@gmail.com>
2026-01-21 07:43:19 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: true,
   modules: [
     '@nuxt/image',
     '@nuxt/ui',
@@ -12,5 +13,10 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   supabase: {
     redirect: false
+  },
+  nitro: {
+    preset: 'netlify',
+    compressPublicAssets: true,
+    minify: true
   },
 })
